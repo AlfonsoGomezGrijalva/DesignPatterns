b6a75a66e88dc39d44e4e4ddcaa22514951e03fb
AlfonsoGomezGrijalva <61067360+AlfonsoGomezGrijalva@users.noreply.github.com>
2020-03-12 15:30:34 -0600

    Facade pattern

---
+++ DesignPatterns/Structural/Facade/WheterServices/EnglishMetricConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Facade.WheterServices
+{
+    public class EnglishMetricConverter
+    {
+        public double FarenheitToCelcious(double degrees)
+        {
+            return ((degrees - 32) / 9.0) * 5.0;
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Facade/WheterServices/GeoLookupService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural.Facade.WheterServices
+{
+    public class GeoLookupService
+    {
+        public Coordinates GetCoordinatesForZipCode(string zipCode)
+        {
+            return new Coordinates()
+            {
+                Latitude = 20.6736,
+                Longitude = -103.344
+            };
+        }
+
+        public class Coordinates
+        {
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+        }
+
+        public string GetCityForZipCode(string zipCode)
+        {
+            return "Guadalajara";
+        }
+
+        public string GetStateForZipCode(string zipCode)
+        {
+            return "Jalisco";
+        }
+    }
+}
